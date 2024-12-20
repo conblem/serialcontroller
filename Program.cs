0cab15a8371d6adbd4d6268c927c12c6ddeba321
conblem <mail@conblem.me>
2024-12-20 14:37:35 +0100

    improve code

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-using serialcontroller;
+﻿using serialcontroller;
 
 using IDriveTrain driveTrain = new UartDriveTrain();
 using IController controller = new Controller();
@@ -11,7 +11,7 @@ controller.DirectionChanged += (_, e) => {
     lastDirectionEvent = e;
 };
 
-controller.ButtonPressed += (_, button) => {
+controller.ButtonPressed += async (_, button) => {
     if(button != Button.A) {
         return;
     }
@@ -21,18 +21,51 @@ controller.ButtonPressed += (_, button) => {
     if(lastDirectionEvent.Direction == Direction.Up) {
         var distance = Convert.ToByte(lastDirectionEvent.Value * 255 / 32767);
         Console.WriteLine("Straight" + distance);
-        driveTrain.Drive(DriveMode.Straight, [distance, distance], 0x00);
+        await driveTrain.Drive(DriveMode.Straight, [distance, distance], 0x00);
     }
     if(lastDirectionEvent.Direction == Direction.Left) {
         var left = Convert.ToByte(128 + (lastDirectionEvent.Value * 127 / 32767));
         Console.WriteLine("Left" + left);
-        driveTrain.Drive(DriveMode.TurnOnPoint, [0x00, left], 0x00);
+        await driveTrain.Drive(DriveMode.TurnOnPoint, [0x00, left], 0x00);
     }
     if(lastDirectionEvent.Direction == Direction.Right) {
         var right = Convert.ToByte(lastDirectionEvent.Value * 128 / 32767);
         Console.WriteLine("Right" + right);
-        driveTrain.Drive(DriveMode.TurnOnPoint, [0x00, right], 0x00);
+        await driveTrain.Drive(DriveMode.TurnOnPoint, [0x00, right], 0x00);
     }
+    Console.WriteLine("Command run successfully");
 };
 
 Console.ReadKey();
+
+// while (true)
+// {
+//     var key = Console.ReadKey();
+//     if (key.Key == ConsoleKey.Q)
+//     {
+//         break;
+//     }
+//     
+//     if (key.Key == ConsoleKey.W)
+//     {
+//         Console.WriteLine("Straight");
+//         await driveTrain.Drive(DriveMode.Straight, [0xFF, 0xFF], 0x00);
+//     }
+//     if (key.Key == ConsoleKey.S)
+//     {
+//         Console.WriteLine("Stop");
+//         await driveTrain.Drive(DriveMode.Straight, [0x00, 0x00], 0x00);
+//     }
+//
+//     if (key.Key == ConsoleKey.A)
+//     {
+//         Console.WriteLine("Left");
+//         await driveTrain.Drive(DriveMode.TurnOnPoint, [0x00, 0xFF], 0x00);
+//     }
+//
+//     if (key.Key == ConsoleKey.D)
+//     {
+//         Console.WriteLine("Right");
+//         await driveTrain.Drive(DriveMode.TurnOnPoint, [0x00, 0x00], 0x00);
+//     }
+// }
